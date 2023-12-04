cdb78c920caa2b6f76b58e304faaba80bc9812ea
Priyanshu <pshekhar830@gmail.com>
2023-12-04 16:21:40 +0530

    able to generate pdf but scrolling experience is not smooth

--- src/testreportlab.py
+++ src/testreportlab.py
@@ -1,11 +1,52 @@
-from pypdf import PdfMerger
+import os
+from reportlab.pdfgen import canvas
+# from reportlab.lib.pagesizes import letter
+from PIL import Image
 
-pdfs = ['ohlc-charts/PDFs/output.pdf', 'ohlc-charts/PDFs/test1.pdf', 'ohlc-charts/PDFs/test2.pdf', 'ohlc-charts/PDFs/test3.pdf']
+def get_png_files_in_directory(directory):
+    png_files = [file for file in os.listdir(directory) if file.lower().endswith('.png')]
+    png_files = sorted(png_files)
+    return png_files
 
-merger = PdfMerger()
+def convert_images_to_pdf(image_directory, output_pdf):
+    image_paths = [os.path.join(image_directory, file) for file in get_png_files_in_directory(image_directory)]
+    # c = canvas.Canvas(output_pdf, pagesize=letter)
+    c = canvas.Canvas(output_pdf)
+    image_paths = image_paths[:60]
+    
+    counter=0
+    for image_path in image_paths:
+        print(image_path)
+        print(counter)
+        counter+=1
+        img = Image.open(image_path)
+        # width, height = letter
+        img_width, img_height = img.size
+        
+        # Calculate the aspect ratio to fit the image within the PDF page
+        # aspect_ratio = img_width / img_height
+        # new_width = min(img_width, width)
+        # new_height = new_width / aspect_ratio
+        
+        # Center the image on the page
+        # x_offset = (width - new_width) / 2
+        # y_offset = (height - new_height) / 2
+        
+        # Draw the image on the PDF
+        # c = canvas.Canvas(output_pdf, pagesize=(img_width, img_height))
+        c.setPageSize((img_width, img_height))
+        c.drawInlineImage(img, 0, 0, width=img_width, height=img_height)
+        
+        # Add a new page for the next image
+        c.showPage()
+    c.save()
 
-for pdf in pdfs:
-    merger.append(pdf)
+if __name__ == "__main__":
+    # Specify the directory containing PNG images
+    image_directory = "ohlc-charts/dw1"
 
-merger.write("ohlc-charts/PDFs/result.pdf")
-merger.close()
+    # Specify the output PDF file
+    output_pdf = "ohlc-charts/PDFs/test.pdf"
+
+    # Convert images to PDF
+    convert_images_to_pdf(image_directory, output_pdf)
