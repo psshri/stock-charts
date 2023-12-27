d9e2d42e893f6f85191a724ee1ce7e31e09d43e3
Priyanshu <pshekhar830@gmail.com>
2023-12-27 19:08:27 +0530

    just run now

--- src/all-nse-stocks/get_ohlc_data.py
+++ src/all-nse-stocks/get_ohlc_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import time
 from datetime import datetime
+import json
 
 fileUrl ='https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz'
 symboldf = pd.read_csv(fileUrl)
@@ -12,6 +13,8 @@ nse_eq.reset_index(drop=True, inplace= True)
 list_trading_symbol = list(nse_eq['tradingsymbol'])
 list_instrument_key = list(nse_eq['instrument_key'])
 list_stock_name = list(nse_eq['name'])
+
+file_path = "data/all-nse-stocks-data.json"
 
 today = datetime.now()
 today = today.strftime('%Y-%m-%d')
@@ -34,25 +37,45 @@ def getMonthlyData(instrument):
     response = requests.get(url, headers=headers)
     return response.json()
 
-
-output = {}
-
-start = time.time()
-
-for i in range(len(list_trading_symbol)):
-    trading_symbol = list_trading_symbol[i]
-    instrument_key = list_instrument_key[i]
+def get_candle_data(i):
     try:
+        trading_symbol = list_trading_symbol[i]
+        instrument_key = list_instrument_key[i]
         weekly_data = getWeeklyData(instrument_key)
         weekly_df = pd.DataFrame(weekly_data['data']['candles'])
         monthly_data = getMonthlyData(instrument_key)
-        monthly_df = pd.DataFrame(monthly_data['data']['candles'])
-        # print(i, trading_symbol, len(weekly_data['data']['candles']))
-        print(f"{i}-{trading_symbol}-weekly-{len(weekly_df)}")
-        print(f"{i}-{trading_symbol}-monthly-{len(monthly_df)}")
-        # print(i, trading_symbol, len(monthly_data['data']['candles']))
-        output[trading_symbol] = (weekly_df, monthly_df)
+        monthly_df = pd.DataFrame(monthly_data['data']['candles'])  
+        return trading_symbol, (weekly_df, monthly_df)
     except:
         print('Error in ', trading_symbol)
+        return None
 
-print(f"{time.time() - start} Sec")
+
+if __name__ == "__main__":
+
+    output = {}
+
+    start = time.time()
+
+    # for i in range(len(list_trading_symbol)):
+    #     trading_symbol = list_trading_symbol[i]
+    #     instrument_key = list_instrument_key[i]
+    #     try:
+    #         weekly_data = getWeeklyData(instrument_key)
+    #         weekly_df = pd.DataFrame(weekly_data['data']['candles'])
+    #         monthly_data = getMonthlyData(instrument_key)
+    #         monthly_df = pd.DataFrame(monthly_data['data']['candles'])
+    #         # print(i, trading_symbol, len(weekly_data['data']['candles']))
+    #         print(f"{i}-{trading_symbol}-weekly-{len(weekly_df)}")
+    #         print(f"{i}-{trading_symbol}-monthly-{len(monthly_df)}")
+    #         # print(i, trading_symbol, len(monthly_data['data']['candles']))
+    #         output[trading_symbol] = (weekly_df, monthly_df)
+    #     except:
+    #         print('Error in ', trading_symbol)
+
+    print(f"{time.time() - start} Sec")
+
+
+    # Writing the dictionary to a JSON file
+    with open(file_path, 'w') as json_file:
+        json.dump(my_dict, json_file)
