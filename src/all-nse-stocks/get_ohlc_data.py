637000d24ee203545cf13f967f16c5f2a0424bdc
Priyanshu <pshekhar830@gmail.com>
2023-12-28 03:09:20 +0530

    basic setup ready, now optimize

--- src/all-nse-stocks/get_ohlc_data.py
+++ src/all-nse-stocks/get_ohlc_data.py
@@ -1,8 +1,14 @@
+# right now, the lines that are commented out, should be uncommented if you want
+# to run this script alone
+# one improvement that can be done is: right now i have to define the nse_eq dataframe
+# in both of the scripts main.py and get_ohlc_data.py
+
 import pandas as pd
 import requests
 import time
 from datetime import datetime
 import json
+from multiprocessing import Pool
 
 fileUrl ='https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz'
 symboldf = pd.read_csv(fileUrl)
@@ -14,15 +20,17 @@ list_trading_symbol = list(nse_eq['tradingsymbol'])
 list_instrument_key = list(nse_eq['instrument_key'])
 list_stock_name = list(nse_eq['name'])
 
-file_path = "data/all-nse-stocks-data.json"
+# start_index = 0
+# end_index = 100
 
 today = datetime.now()
 today = today.strftime('%Y-%m-%d')
-from_date = '2014-01-01'
+from_date_weekly = '2018-01-01'
+from_date_monthly = '2014-01-01'
 base_url = "https://api.upstox.com/v2/"
 
 def getWeeklyData(instrument):
-    url = f'{base_url}historical-candle/{instrument}/week/{today}/{from_date}' 
+    url = f'{base_url}historical-candle/{instrument}/week/{today}/{from_date_weekly}' 
     headers = {
         'accept': 'application/json',
     }
@@ -30,7 +38,7 @@ def getWeeklyData(instrument):
     return response.json()
 
 def getMonthlyData(instrument):
-    url = f'{base_url}historical-candle/{instrument}/month/{today}/{from_date}' 
+    url = f'{base_url}historical-candle/{instrument}/month/{today}/{from_date_monthly}' 
     headers = {
         'accept': 'application/json',
     }
@@ -43,39 +51,37 @@ def get_candle_data(i):
         instrument_key = list_instrument_key[i]
         weekly_data = getWeeklyData(instrument_key)
         weekly_df = pd.DataFrame(weekly_data['data']['candles'])
+        weekly_df.columns = ['Datetime', 'Open', 'High', 'Low', 'Close', 'Volume', 'NA']
+        weekly_df['Datetime'] = pd.to_datetime(weekly_df['Datetime'])
+        weekly_df.set_index('Datetime', inplace=True)
+        weekly_df = weekly_df.iloc[::-1] # reverse the df
         monthly_data = getMonthlyData(instrument_key)
-        monthly_df = pd.DataFrame(monthly_data['data']['candles'])  
+        monthly_df = pd.DataFrame(monthly_data['data']['candles'])
+        monthly_df.columns = ['Datetime', 'Open', 'High', 'Low', 'Close', 'Volume', 'NA']
+        monthly_df['Datetime'] = pd.to_datetime(monthly_df['Datetime'])
+        monthly_df.set_index('Datetime', inplace=True)
+        monthly_df = monthly_df.iloc[::-1]
         return trading_symbol, (weekly_df, monthly_df)
     except:
         print('Error in ', trading_symbol)
         return None
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    output = {}
+#     output = {}
 
-    start = time.time()
+#     start = time.time()
 
-    # for i in range(len(list_trading_symbol)):
-    #     trading_symbol = list_trading_symbol[i]
-    #     instrument_key = list_instrument_key[i]
-    #     try:
-    #         weekly_data = getWeeklyData(instrument_key)
-    #         weekly_df = pd.DataFrame(weekly_data['data']['candles'])
-    #         monthly_data = getMonthlyData(instrument_key)
-    #         monthly_df = pd.DataFrame(monthly_data['data']['candles'])
-    #         # print(i, trading_symbol, len(weekly_data['data']['candles']))
-    #         print(f"{i}-{trading_symbol}-weekly-{len(weekly_df)}")
-    #         print(f"{i}-{trading_symbol}-monthly-{len(monthly_df)}")
-    #         # print(i, trading_symbol, len(monthly_data['data']['candles']))
-    #         output[trading_symbol] = (weekly_df, monthly_df)
-    #     except:
-    #         print('Error in ', trading_symbol)
+#     l = list(range(len(list_trading_symbol[start_index:end_index])))
 
-    print(f"{time.time() - start} Sec")
+#     with Pool(processes=4) as pool:
+#         results = pool.map(get_candle_data, l)
+
+#     for result in results:
+#         if result is not None:
+#             trading_symbol, (weekly_df, monthly_df) = result
+#             output[trading_symbol] = [weekly_df, monthly_df]
 
 
-    # Writing the dictionary to a JSON file
-    with open(file_path, 'w') as json_file:
-        json.dump(my_dict, json_file)
+#     print(f"{time.time() - start} Sec")
---
+++ src/all-nse-stocks/main.py
@@ -0,0 +1,79 @@
+from get_ohlc_data import get_candle_data
+from create_candlestick_chart import download_image
+import pandas as pd
+import requests
+import time
+from datetime import datetime
+import json
+from multiprocessing import Pool
+import os
+from create_pdf import convert_images_to_pdf
+import asyncio
+import nest_asyncio
+nest_asyncio.apply()
+from send_pdf import sendit
+
+start_index = 400
+end_index = 500
+
+# start_index = 0
+# end_index = 100
+folder_name = f"{start_index}-{end_index}"
+os.makedirs(f"ohlc-charts/{folder_name}", exist_ok=True)
+
+fileUrl ='https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz'
+symboldf = pd.read_csv(fileUrl)
+symboldf['expiry'] = pd.to_datetime(symboldf['expiry']).apply(lambda x: x.date())
+nse_eq = symboldf[(symboldf.last_price.notnull()) & (symboldf.exchange == 'NSE_EQ') & (symboldf.tick_size == 0.05)]
+nse_eq.reset_index(drop=True, inplace= True)
+
+list_trading_symbol = list(nse_eq['tradingsymbol'])
+list_instrument_key = list(nse_eq['instrument_key'])
+list_stock_name = list(nse_eq['name'])
+
+if __name__ == "__main__":
+
+    # output = {}
+
+    start = time.time()
+
+    # l = list(range(len(list_trading_symbol[start_index:end_index])))
+    l = list(range(start_index, end_index))
+
+    with Pool(processes=4) as pool:
+        results = pool.map(get_candle_data, l)
+
+    for result in results:
+        if result is not None:
+            trading_symbol, (weekly_df, monthly_df) = result
+            # output[trading_symbol] = [weekly_df, monthly_df]
+            download_image('2weekly', weekly_df, trading_symbol, folder_name)
+            download_image('1monthly', monthly_df, trading_symbol, folder_name)
+
+
+            
+    # Specify the directory containing PNG images
+    image_directory = f"ohlc-charts/{folder_name}"
+
+    # Specify the output PDF file
+    output_pdf = f"ohlc-charts/PDFs/{folder_name}.pdf"
+
+    # CREATE PDF
+    print("Creating PDF...")
+
+    # Convert images to PDF
+    convert_images_to_pdf(image_directory, output_pdf)
+
+    print("PDF creation completed!")
+
+    # SEND PDF
+    print("Sending PDFs to Telegram...")
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(sendit(output_pdf))
+
+    print("PDFs sent to Telegram!")
+
+
+
+    print(f"{time.time() - start} Sec")
